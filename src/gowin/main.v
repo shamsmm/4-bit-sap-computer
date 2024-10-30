af9419bf22487787ebce69517056c5c971e3bf99
Shams M. Monem <shamsmmonem@gmail.com>
2024-10-31 00:34:08 +0300

    Fixed FPGA LED segment

--- src/gowin/main.v
+++ src/gowin/main.v
@@ -2,10 +2,6 @@ module main(clk, rst, port_out);
 
 input clk, rst;
 output [3:0] port_out;
-wire [3:0] cpu_port_out;
-
-// LEDs are active LOW
-assign port_out = ~cpu_port_out;
 
 gowin_clkdiv8 div1(clk8, clk, rst);
 gowin_clkdiv8 div2(clk64, clk8, rst);
@@ -14,6 +10,6 @@ gowin_clkdiv8 div4(clk4096, clk512, rst);
 gowin_clkdiv8 div5(clk32786, clk4096, rst);
 gowin_clkdiv8 div6(clk262144, clk32786, rst);
 
-cpu my_computer(clk262144, rst, cpu_port_out);
+cpu my_computer(clk262144, rst, port_out);
 
 endmodule
