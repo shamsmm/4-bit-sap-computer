d7d4946cfa72e7393fd4052a7713a0ac541bf117
Shams M. Monem <shamsmmonem@gmail.com>
2024-10-30 10:51:46 +0300

    Used gowin IP ROM16

--- src/cpu_mem_rom.v
+++ src/cpu_mem_rom.v
@@ -1,12 +1,3 @@
-`define R1DATA 16'b0
-`define R2DATA 16'b0
-`define R3DATA 16'b0
-`define R4DATA 16'b0
-`define R5DATA 16'b0
-`define R6DATA 16'b0
-`define R7DATA 16'b0
-`define R8DATA 16'b0
-
 module cpu_mem_rom(bus_out, address);
 input [3:0] address;
 output [7:0] bus_out;
@@ -15,13 +6,6 @@ assign bus_out = rom_out;
 
 wire [7:0] rom_out;
 
-ROM16 #(.INIT_0(`R1DATA)) r1(.AD(address), .DO(rom_out[0]));
-ROM16 #(.INIT_0(`R2DATA)) r2(.AD(address), .DO(rom_out[1]));
-ROM16 #(.INIT_0(`R3DATA)) r3(.AD(address), .DO(rom_out[2]));
-ROM16 #(.INIT_0(`R4DATA)) r4(.AD(address), .DO(rom_out[3]));
-ROM16 #(.INIT_0(`R5DATA)) r5(.AD(address), .DO(rom_out[4]));
-ROM16 #(.INIT_0(`R6DATA)) r6(.AD(address), .DO(rom_out[5]));
-ROM16 #(.INIT_0(`R7DATA)) r7(.AD(address), .DO(rom_out[6]));
-ROM16 #(.INIT_0(`R8DATA)) r8(.AD(address), .DO(rom_out[7]));
+gowin_rom16 rom(rom_out, address);
 
 endmodule
---
+++ src/gowin_rom16/gowin_rom16.v
@@ -0,0 +1,71 @@
+//Copyright (C)2014-2024 Gowin Semiconductor Corporation.
+//All rights reserved.
+//File Title: IP file
+//Tool Version: V1.9.10.03
+//Part Number: GW1NR-LV9QN88PC6/I5
+//Device: GW1NR-9
+//Device Version: C
+//Created Time: Wed Oct 30 10:50:22 2024
+
+module gowin_rom16 (dout, ad);
+
+output [7:0] dout;
+input [3:0] ad;
+
+ROM16 rom16_inst_0 (
+    .DO(dout[0]),
+    .AD(ad[3:0])
+);
+
+defparam rom16_inst_0.INIT_0 = 16'h0200;
+
+ROM16 rom16_inst_1 (
+    .DO(dout[1]),
+    .AD(ad[3:0])
+);
+
+defparam rom16_inst_1.INIT_0 = 16'h0004;
+
+ROM16 rom16_inst_2 (
+    .DO(dout[2]),
+    .AD(ad[3:0])
+);
+
+defparam rom16_inst_2.INIT_0 = 16'h0000;
+
+ROM16 rom16_inst_3 (
+    .DO(dout[3]),
+    .AD(ad[3:0])
+);
+
+defparam rom16_inst_3.INIT_0 = 16'h0020;
+
+ROM16 rom16_inst_4 (
+    .DO(dout[4]),
+    .AD(ad[3:0])
+);
+
+defparam rom16_inst_4.INIT_0 = 16'h074A;
+
+ROM16 rom16_inst_5 (
+    .DO(dout[5]),
+    .AD(ad[3:0])
+);
+
+defparam rom16_inst_5.INIT_0 = 16'h0389;
+
+ROM16 rom16_inst_6 (
+    .DO(dout[6]),
+    .AD(ad[3:0])
+);
+
+defparam rom16_inst_6.INIT_0 = 16'h06F4;
+
+ROM16 rom16_inst_7 (
+    .DO(dout[7]),
+    .AD(ad[3:0])
+);
+
+defparam rom16_inst_7.INIT_0 = 16'h071A;
+
+endmodule //gowin_rom16
